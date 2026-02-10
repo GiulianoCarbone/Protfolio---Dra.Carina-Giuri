bac1d3ea4da84d6b0ab02b166acd706e03cc2ab2
Giuliano Carbone <benjacarbone@gmail.com>
2026-02-10 15:59:14 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * JavaScript personalizado para el Portfolio de la Dra. Carina Giuri
+ * Aquí puedes gestionar menús, modales y otras interacciones.
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio scripts cargados correctamente.');
+});
